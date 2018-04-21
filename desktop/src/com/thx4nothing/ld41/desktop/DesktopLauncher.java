eed2303a413293728c57208e4c8f906c8323145a
Marlon Beck <thx4nothing15@gmx.de>
2018-04-21 10:03:59 +0900

    init libgdx

---
+++ desktop/src/com/thx4nothing/ld41/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.thx4nothing.ld41.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.thx4nothing.ld41.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
